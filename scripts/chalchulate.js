a72c33691e51aed9c2e1256129a54315ddac92eb
angad trehan <84653725+angadtrehan@users.noreply.github.com>
2025-03-04 12:46:30 +0530

    Implement the actual math
    Break your brain and make a lot of mistakes before implementing simple
    math you dumbo.

---
+++ scripts/chalchulate.js
@@ -0,0 +1,118 @@
+const result = document.querySelector(".result p");
+const buttons = document.querySelector(".buttons");
+const body = document.querySelector("body");
+const DEFAULT_NUM = 0;
+const DEFAULT_OP = "plus"
+
+let left = DEFAULT_NUM;
+let right = DEFAULT_NUM;
+let operator = DEFAULT_OP;
+let newNum = true;
+let idiotFlag = false;
+
+function wrapInDisplay(num) {
+    if (num.indexOf(".") !== -1 && num.length > 10) {
+        let numArr = num.split('');
+        if (numArr[10] === ".") {
+            if (Number(numArr[11]) >= 5) {
+                numArr[9] = Number(numArr[9]) + 1;
+            }
+            return numArr.slice(0, 10).join('');
+        }
+        if (numArr[9] === ".") {
+            numArr[8] = Number(numArr[8]) + 1;
+            return numArr.slice(0, 9).join('');
+        }
+        if (Number(numArr[10]) >= "5") {
+            numArr[9] = Number(numArr[9]) + 1;
+        }
+        return numArr.slice(0, 10).join('');
+    }
+    return num;
+}
+function handleNumberInput(number) {
+    result.textContent = (newNum) ? (number === ".") ? result.textContent + number : number : (result.textContent.length === 10) ? result.textContent : result.textContent + number;
+    newNum = false;
+    right = parseFloat(result.textContent);
+    console.log(right);
+}
+function handleOperation(operation) {
+    let res = `${left}`;
+    switch(operator) {
+        case "plus":
+            res = `${left + right}`;
+            break;
+        case "minus":
+            res = `${left - right}`;
+            break;
+        case "mult":
+            res = `${left * right}`;
+            break;
+        case "divide":
+            if (right === 0) {
+                res = 'idiot.';
+                idiotFlag = true;
+            } else {
+                res = `${left / right}`;
+            }
+            break;
+        case "equals":
+            break;
+
+    }
+    operator = operation;
+    right = DEFAULT_NUM;
+    result.textContent = wrapInDisplay(res);
+    left = (idiotFlag) ? DEFAULT_NUM : parseFloat(result.textContent);
+    console.log(`${left} is left`);
+    newNum = true;
+}
+function handleFunction(func) {
+    if (func === "reset") {
+        left = DEFAULT_NUM;
+        right = DEFAULT_NUM;
+        operator = DEFAULT_OP;
+        newNum = true;
+        result.textContent = left;
+        idiotFlag = false;
+        removeHighlights();
+        console.log("resettededd");
+    } else  {
+        alert("This doesn't do anything right now. The logic is clear but I really just want to move ahead pls");
+    }
+}
+function isIdiot(button) {
+    return idiotFlag && button.id !== "reset";
+}
+function highlightResetButton() {
+    body.classList.add("highlight");
+    for (const button of buttons.children) {
+        if (button.id === "reset") {
+            button.classList.add("highlight");
+        } else {
+            button.classList.add("grey-out");
+        }
+    }
+}
+function removeHighlights() {
+    body.classList.remove("highlight");
+    for (const button of buttons.children) {
+        button.classList.remove("highlight");
+        button.classList.remove("grey-out");
+    }
+}
+
+export function handleButtonPress(button) {
+    if (!isIdiot(button)) {
+        if (button.classList.contains("inputs")) {
+            handleNumberInput(button.textContent);
+        } else if (button.classList.contains("operator")) {
+            handleOperation(button.id);
+        } else if (button.classList.contains("func")) {
+            handleFunction(button.id);
+        }
+    } else {
+        result.textContent = "reset now";
+        highlightResetButton();
+    }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
-const blinker = document.querySelector(".result > span");
+import { handleButtonPress } from "./chalchulate.js";
+
+const blinker = document.querySelector(".result span");
 
 function toggleBlinker() {
     blinker.classList.toggle("blinker");
@@ -16,6 +18,7 @@ function selectOperator(button) {
     const operators = document.querySelectorAll(".operator");
     for (const op of operators) {
         if (op.id === button.id && button.id !== "equals") {
+            removePressed(op);
             addPressed(op);
         } else {
             removePressed(op);
@@ -25,21 +28,19 @@ function selectOperator(button) {
 
 const buttons = document.querySelector(".buttons");
 for (const button of buttons.children) {
+    button.addEventListener('click', () => {
+        handleButtonPress(button);
+    });
     if (button.classList.contains("operator") && button.id !== "equals") {
         button.addEventListener('click', () => {
             selectOperator(button);
         });
-    } else if (button.id === "equals") {
-        button.addEventListener('click', () => {
-            selectOperator(button);
-        })
-        button.addEventListener('mousedown', () => {
-            button.classList.add("pressed-button");
-            setTimeout(() => {
-                removePressed(button);
-            }, 200);
-        });
     } else {
+        if (button.id === "equals" || button.id === "reset") {
+            button.addEventListener('click', () => {
+                selectOperator(button);
+            })
+        }
         button.addEventListener('mousedown', () => {
             button.classList.add("pressed-button");
             setTimeout(() => {
